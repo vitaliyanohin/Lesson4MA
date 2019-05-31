73000237a03abe31fad2730ed9f09181414d6c67
Vitaliy <vitaliyanohin@gmail.com>
2019-05-31 12:29:56 +0300

    develop commit

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -23,6 +23,12 @@ public class MainApplication {
     System.out.println("Next:");
     System.out.println(asciiCharSequence);
     System.out.println(asciiCharSequence.subSequence(20, 94));
+    System.out.println("asciiCharSequence Output: ! \" # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ;" +
+            " < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \\ ] ^ _ ` a b c d e f g h i"
+            + " j k l m n o p q r s t u v w x y z { | } ~");
+    System.out.println("aasciiCharSequence.subSequence(20, 94) Output: !5 6 7 8 9 : ; < = > ?"
+            + " @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \\ ] ^ _ `"
+            + " a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~ ");
   }
 
   private static TextAnalyzer.Label checkLabels(TextAnalyzer[] analyzers, String text) {
